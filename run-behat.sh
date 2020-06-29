4c747577a31c87592f81efc4dfc8ccee0c948d66
Youri Westerman <youri.westerman@nedac.eu>
2020-06-29 17:59:36 +0200

    placed cd inside condition

--- run-behat.sh
+++ run-behat.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-cd tests/Application
-if [[ $(git status | awk '{print $3}' | head -n1) == "1.7-bootstrap-theme" ]]; then
+if [[ $(cd tests/Application && git status | awk '{print $3}' | head -n1) == "1.7-bootstrap-theme" ]]; then
 	vendor/bin/behat --tags=@theme_setup
 	(cd tests/Application && bin/console ca:cl)
 	vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction || vendor/bin/behat --tags="@theme" --strict -vvv --no-interaction --rerun
