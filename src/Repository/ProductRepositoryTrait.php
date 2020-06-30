6308c9f690b96c0132536bf57fd99d373be62bce
Youri Westerman <youri.westerman@nedac.eu>
2020-06-30 16:47:20 +0200

    Removing hack from query (we need all the variants)

---
+++ src/Repository/ProductRepositoryTrait.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nedac\SyliusTemporarilyOutOfStockPlugin\Repository;
+
+use Doctrine\ORM\QueryBuilder;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\TaxonInterface;
+
+/**
+ * @method QueryBuilder createQueryBuilder(string $alias, ?string $indexBy = null)
+ */
+trait ProductRepositoryTrait
+{
+    public function createShopListQueryBuilder(
+        ChannelInterface $channel,
+        TaxonInterface $taxon,
+        string $locale,
+        array $sorting = [],
+        bool $includeAllDescendants = false
+    ): QueryBuilder {
+        $queryBuilder = $this->createQueryBuilder('o')
+            ->distinct()
+            ->addSelect('translation')
+            ->addSelect('productTaxon')
+            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->innerJoin('o.productTaxons', 'productTaxon');
+
+        if ($includeAllDescendants) {
+            $queryBuilder
+                ->innerJoin('productTaxon.taxon', 'taxon')
+                ->andWhere('taxon.left >= :taxonLeft')
+                ->andWhere('taxon.right <= :taxonRight')
+                ->andWhere('taxon.root = :taxonRoot')
+                ->setParameter('taxonLeft', $taxon->getLeft())
+                ->setParameter('taxonRight', $taxon->getRight())
+                ->setParameter('taxonRoot', $taxon->getRoot())
+            ;
+        } else {
+            $queryBuilder
+                ->andWhere('productTaxon.taxon = :taxon')
+                ->setParameter('taxon', $taxon)
+            ;
+        }
+
+        $queryBuilder
+            ->andWhere(':channel MEMBER OF o.channels')
+            ->andWhere('o.enabled = true')
+            ->setParameter('locale', $locale)
+            ->setParameter('channel', $channel)
+        ;
+
+        // Grid hack, we do not need to join these if we don't sort by price
+        if (isset($sorting['price'])) {
+            $queryBuilder
+                ->addSelect('variant')
+                ->addSelect('channelPricing')
+                ->innerJoin('o.variants', 'variant')
+                ->innerJoin('variant.channelPricings', 'channelPricing')
+                ->andWhere('channelPricing.channelCode = :channelCode')
+                ->setParameter('channelCode', $channel->getCode())
+            ;
+        }
+
+        return $queryBuilder;
+    }
+}
